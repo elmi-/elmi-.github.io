6663608a3a3f8f21e031ca24e5315f0cc601e414
elmi- <abdi.elmi@rocketmail.com>
2026-05-16 19:02:17 -0400

    added logic for changing my shirt color

--- scripts/theme-switcher.js
+++ scripts/theme-switcher.js
@@ -1,26 +1,41 @@
-function setAccent(color) {
-  document.documentElement.style.setProperty(
-    '--accent-color',
-    color
-  );
-
-  localStorage.setItem('accent-color', color);
-}
-
-window.onload = () => {
-  const savedColor =
-    localStorage.getItem('accent-color');
-
-  if (savedColor) {
-    setAccent(savedColor);
+const accents = {
+  green: {
+    color: "#008073",
+    background: "../img/outdoor.jpg"
+  },
+  blue: {
+    color: "#2980b9",
+    background: "../img/outdoor-blue.png"
+  },
+  red: {
+    color: "#c0392b",
+    background: "../img/outdoor-red.png"
   }
 };
 
-const themeButtons = document.querySelectorAll('.theme-switcher button');
+function setAccent(accent) {
+  const theme = accents[accent];
 
-themeButtons.forEach(btn => {
-  btn.addEventListener('click', () => {
-    themeButtons.forEach(b => b.classList.remove('active'));
-    btn.classList.add('active');
-  });
-});
+  if (!theme) return;
+
+  document.documentElement.style.setProperty(
+    '--accent-color',
+    theme.color
+  );
+
+  const intro = document.querySelector("#intro.section");
+
+  if (intro) {
+    intro.style.backgroundImage =
+      `url(${theme.background})`;
+  }
+
+  localStorage.setItem('accent-color', accent);
+}
+
+window.onload = () => {
+  const savedAccent =
+    localStorage.getItem('accent-color') || 'green';
+
+  setAccent(savedAccent);
+};
